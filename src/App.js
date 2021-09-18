ec2521b8ad0aeb9bb79cf4525cce2795f65b4674
babulakterfsd <babulakterfsd@gmail.com>
2021-09-18 17:49:26 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
@@ -17,29 +18,73 @@ function App() {
       devType: 'Javascript developer',
       salary: '$400k',
       isSenior: true
-    },
-    {
-      name: 'Babul Akter',
-      age: 26,
-      devType: 'front end developer',
-      salary: '$100k',
-      isSenior: false
     }
   ]
 
   return (
-     <div className="webDevs">
+     <>
+       <h1 className="text-center">Developer List</h1>
+       <div className="d-flex justify-content-around">
+       <hr />
           {devArray.map(dev => <Devs name={dev.name} age={dev.age} devType={dev.devType} salary={dev.salary} isSenior={dev.isSenior}></Devs>)}
+       </div>
+
+       <div className="learnState border border-success border-2 w-50 mx-auto m-2 mb-5">
+         <LearnState></LearnState>
+       </div>
+
+
+       <Loadusers></Loadusers>
+     </>
+  )
+}
+
+function Loadusers(props) {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+     <div className="mt-5 py-5 w-50 mx-auto border border-primary border-3">
+       <h2 className="text-center">Load users from externeal data</h2>
+       <hr />
+       {users.map(user => GenerateUsers(user))}
      </div>
   )
 }
+function GenerateUsers(props) {
+  const {name, id} = props
+  return (
+    <>
+     <h4 className="text-center"><span className="text-danger">{id}.</span> {name}</h4>
+    </>
+  )
+}
+
+
+
+function LearnState(props) {
+  let [count, setCount] = useState(0);
+  const increaser = () => setCount(count + 1)
+  const decreaser = () => setCount(count - 1)
+     return (
+       <div className="d-flex justify-content-center align-items-center py-5 px-3">
+         <button className="btn btn-success btn-sm me-5 py-0" onClick={increaser}>increase</button>
+        <h3 className="text-center">Count: {count}</h3>
+        <button className="btn btn-danger ms-5 py-0" onClick={decreaser}>decrease</button>
+       </div>
+     )
+}
+
 
 function Devs(props) {
   const {name, age, devType, salary, isSenior} = props
   let senior = isSenior;
    senior = senior ? `A senior developer` : `A junior developer`
   return (
-    <div className="devContainer text-center border border-warning rounded-2 p-2 w-50 mx-auto mb-2">
+    <div className="dev border border-warning rounded-2 p-5 mb-2">
       <h2> name: {name} </h2>
       <h3> age: {age}</h3>
       <h4>type: {devType}</h4>
